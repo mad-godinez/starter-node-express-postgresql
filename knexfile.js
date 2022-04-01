1dcb940ec9dd7b78ed8b92b85fa8efd6a209066e
Mad Godinez <madgodinez@gmail.com>
2022-04-01 10:34:25 -0500

    made server connection details private

--- knexfile.js
+++ knexfile.js
@@ -2,16 +2,6 @@ const path = require("path");
 require("dotenv").config();
 const { DATABASE_URL } = process.env;
 
-// Define the database configuration object
-const config = {
-  client: "postgresql",
-  connection:
-    "postgres://myfakedatabase:8t6FiWqSDF8GsR--7mrun245I9TofnWd@fakepostgres.db.elephantsql.com:5432/myfakedatabase",
-};
-
-// Initialize a Knex instance by passing `config` as an argument to the Knex module
-const knex = require("knex")(config);
-
 module.exports = {
   development: {
     client: "postgresql",
